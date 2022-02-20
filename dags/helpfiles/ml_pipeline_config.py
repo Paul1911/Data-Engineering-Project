7a30df0f824d1589c2179825db667f9e20bc7040
Paul Friedrich <paul.friedrich@iubh.de>
2022-02-20 11:37:55 +0100

    dags added

--- dags/helpfiles/ml_pipeline_config.py
+++ dags/helpfiles/ml_pipeline_config.py
@@ -6,5 +6,5 @@ params = {
     "db_results_table": "results",
     "test_split_ratio": 0.2,
     "cv_folds": 5,
-    "logreg_maxiter": 1000
+    "logreg_maxiter": 1000 #Todo: check what you need here and adjust file
 }   
