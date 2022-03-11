236cac1d10e134aba9a788896aed5494434c7032
Paul Friedrich <paul.friedrich@iubh.de>
2022-03-11 18:18:56 +0100

    fixed SQL statements

--- dags/sql/copy_df_to_db.sql
+++ dags/sql/copy_df_to_db.sql
@@ -1,3 +1,3 @@
-COPY raw_data
+COPY raw_data (transaction_time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount,Class,Datetime_write)
 FROM '/maindata/df_raw.csv'
 DELIMITER ',' CSV HEADER;
--- dags/sql/copy_target_prediction_to_db.sql
+++ dags/sql/copy_target_prediction_to_db.sql
@@ -1,3 +1,3 @@
-COPY target_prediction
+COPY target_prediction (experiment_date, actual_class, predicted_class)
 FROM '/maindata/target_prediction.csv'
 DELIMITER ',' CSV HEADER;
