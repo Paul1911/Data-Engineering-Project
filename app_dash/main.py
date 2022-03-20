2e054267b3aa9427c97254e5db710b2555680871
Paul Friedrich <paul.friedrich@iubh.de>
2022-03-20 11:24:59 +0100

    modified visualisations

--- app_dash/main.py
+++ app_dash/main.py
@@ -27,8 +27,22 @@ dash_app.config.suppress_callback_exceptions = True
 #function approach 
 def serve_layout():
     #Read tables
-    results_from_db = pd.read_sql_table('results', con = engine)
+    #raw_data_from_db = pd.read_sql_table('raw_data', con = engine, columns = ['Class']) 
+    '''This approach of raw data visualization is not working as nginx times out. Even when working with SQL queries, it takes ages.
+    For the time being, we stay with the metrics visualization.'''
     target_prediction_from_db = pd.read_sql_table('target_prediction', con = engine)
+
+    #Raw data check
+    #exp_date = pd.read_sql('SELECT DISTINCT Datetime_write FROM raw_data', engine)
+    #print(exp_date)
+    #observation_count = []
+    #nan_rows = []
+    #fraud_count = []
+    #for timestamp in raw_data_from_db['Datetime_write'].unique():
+    #    df = raw_data_from_db[raw_data_from_db['Datetime_write'] == timestamp]
+    #    observation_count.append(len(df.index))
+    #    nan_rows.append(len(df[df.isna().any(axis=1)]))
+    #    fraud_count.append(df['Class'].sum())
 
     #Metric calculations
     exp_date = [event for event in target_prediction_from_db['experiment_date'].unique()]
@@ -36,7 +50,6 @@ def serve_layout():
     precision = []
     accuracy = []
 
-    #def metric_calculations():
     for timestamp in target_prediction_from_db['experiment_date'].unique():
         df = target_prediction_from_db[target_prediction_from_db['experiment_date'] == timestamp ]
         df = df.astype({'actual_class': int, 'predicted_class': int})
@@ -54,27 +67,46 @@ def serve_layout():
 
 
     return html.Div(
-#dash_app.layout = html.Div( #traditional approach
     children=[
         html.H1(children="Model Training Monitor"),
-        html.H2(children="Raw Data Quality Montitoring"),
-        html.H2(children="Model Result Montitoring"),
+        #html.H2(children="Raw Data Quality Montitoring"),
         html.Div(children="""Displaying the PostgreSQL-data in a Dash WebApp"""),
-        dcc.Graph(
-            id="example-graph",
-            figure={
-                "data": [
-                    {"x": [1, 2, 3], "y": [4, 15, 2], "type": "bar", "name": "Like"},
-                    {
-                        "x": [1, 2, 3],
-                        "y": [2, 4, results_from_db.iloc[-1,-1]],
-                        "type": "bar",
-                        "name": "Comment",
-                    },
-                ],
-                "layout": {"title": "Like Vs Comment Dash Visualization"},
-            },
-        ),
+        #Start of the raw data visualization
+        #html.Div([
+        #    dcc.Graph(
+        #        id="raw_data_viz",
+        #        figure={
+        #            "data": [
+        #                {"x": [1, 2, 3], "y": [4, 15, 2], "type": "bar", "name": "Like"},
+        #                {
+        #                    "x": [1, 2, 3],
+        #                    "y": [2, 4, 5],
+        #                    "type": "bar",
+        #                    "name": "Comment",
+        #                },
+        #            ],
+        #            "layout": {"title": "Like Vs Comment Dash Visualization"},
+        #        },
+        #        className='six columns', #to display both graphs in one column
+        #    ),
+        #    dcc.Graph(
+        #        id="raw_data_viz2",
+        #        figure={
+        #            "data": [
+        #                {"x": [1, 2, 3], "y": [4, 15, 2], "type": "bar", "name": "Like"},
+        #                {
+        #                    "x": [1, 2, 3],
+        #                    "y": [2, 4, 5],
+        #                    "type": "bar",
+        #                    "name": "Comment",
+        #                },
+        #            ],
+        #            "layout": {"title": "Like Vs Comment Dash Visualization"},
+        #        },
+        #        className='six columns',
+        #    ),
+        #]),''',
+        html.H2(children="Model Result Montitoring"),
         dcc.Graph(
             id="model_metrics",
             figure={
@@ -86,56 +118,10 @@ def serve_layout():
                 "layout": {"title": "Performance Metrics of Model Training over Recent Training Runs"},
             },
         ),
-    ]
-)
+    ])
 
 #function approach
 dash_app.layout = serve_layout
-
-""" @dash_app.callback(dash.dependencies.Output('model_metrics', 'figure'),
-#    [dash.dependencies.Input('refresh_data', 'n_clicks')])
-    [dash.dependencies.Input('interval-component', 'n_intervals')])
-
-def update_model_metrics():
-    target_prediction_from_db = pd.read_sql_table('target_prediction', con = engine)
-
-    #Metric calculations
-    exp_date = [event for event in target_prediction_from_db['experiment_date'].unique()]
-    recall = []
-    precision = []
-    accuracy = []
-    roc_auc = []
-
-    for timestamp in target_prediction_from_db['experiment_date'].unique():
-        df = target_prediction_from_db[target_prediction_from_db['experiment_date'] == timestamp ]
-        df = df.astype({'actual_class': int, 'predicted_class': int})
-        print(df.dtypes)
-        print(df.head())
-        #recall
-        recall_df = metrics.recall_score(df['actual_class'],df['predicted_class'])
-        recall.append(recall_df)
-        #precision
-        precision_df = metrics.precision_score(df['actual_class'],df['predicted_class'])
-        precision.append(precision_df)
-        #accuracy
-        accuracy_df = metrics.accuracy_score(df['actual_class'],df['predicted_class'])
-        accuracy.append(accuracy_df)
-        #roc_auc
-        roc_auc_df = metrics.roc_auc_score(df['actual_class'],df['predicted_class'])
-        roc_auc.append(roc_auc_df)
-
-        #Create Figure
-        figure={
-                "data": [
-                    {"x": exp_date, "y": recall, "type": "line", "name": "Recall"},
-                    {"x": exp_date, "y": precision, "type": "line", "name": "Precision"},
-                    {"x": exp_date, "y": accuracy, "type": "line", "name": "Accuracy"},
-                ],
-                "layout": {"title": "Performance Metrics of Model Training over Recent Training Runs"},
-            },
-
-        # Create plotly figure
-    return figure """
 
 
 if __name__ == '__main__':
