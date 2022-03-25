941295c3e46ebf338984dd4c98f48dbf1fb5f449
Paul Friedrich <paul.friedrich@iubh.de>
2022-03-25 14:35:13 +0100

    removed df size reduction for testing

--- dags/helpfiles/import_data.py
+++ dags/helpfiles/import_data.py
@@ -5,9 +5,9 @@ from helpfiles.temp_save_load import save_files
 def load_data():
     '''Loads credit fraud data and doubles its observations until 1,000,000 lines are reached 
     as this is a requirement for the project. Afterwards, it is saved in temp'''
-    df = pd.read_csv("/opt/airflow/data/creditcard.csv").head(10000) # Todo: remove facilitation
-    #while len(df.index) < 1000000:
-    #    df = pd.concat([df,df])
+    df = pd.read_csv("/opt/airflow/data/creditcard.csv").head(10000)
+    while len(df.index) < 1000000:
+        df = pd.concat([df,df])
     df.reset_index(drop = True, inplace = True)
     df.name = 'df'
 
